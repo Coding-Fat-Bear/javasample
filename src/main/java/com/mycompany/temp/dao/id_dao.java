25e751a10f3c68ec05b97f5a556b28f7930c4c8d
AGI03 <AGI03@AGI003>
2022-02-25 09:41:39 +0530

    version 1.6  repo update task

--- src/main/java/com/mycompany/temp/dao/id_dao.java
+++ src/main/java/com/mycompany/temp/dao/id_dao.java
@@ -6,7 +6,6 @@
 package com.mycompany.temp.dao;
 
 import com.mycompany.temp.model.id;
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -21,9 +20,6 @@ public class id_dao {
         id a = new id();
         try
         {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://agn.db.mysql.agnieindia.com:3306/agnieportal","db_user","AgnChennai@2021");
-            Statement st = con.createStatement();
             ResultSet rs = st.executeQuery("select * from Login_Master where LOGINID=" + aid);
            if( rs.next())
            {
